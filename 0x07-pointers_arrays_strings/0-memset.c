0f545a8af93f1960bcf7e339364358152f3ac046
ann <113899485+50383668@users.noreply.github.com>
2023-10-23 22:12:11 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 /**
-*_memset - fills memory with a constant byte
-*@s: source string
-*@b: content byte for filling
-*@n: length of buffer
-*Return: new string
-*/
+ * _memset - sets memory to array
+ * @s: array to set
+ * @b: value to set it as
+ * @n: n amount of times
+ * Return: char value of s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0;
-	
-while (i < n)
-{
-*(s + i) = b;
-i++;
-}
-return (s);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
