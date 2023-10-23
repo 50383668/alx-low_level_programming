36365e14b88f2d946974a8fc19d0d8dc05cff5fb
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 00:51:11 +0300

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main-very boring
+ * @parametrix: parameter string c
+ *Return: Return value is zer0
+ */
+int main(void)
+{
+	char c[100] = "with proper grammar, but the outcome is a piece of art,\n";
+
+		printf("%s",c);
+	return (0);
+}
