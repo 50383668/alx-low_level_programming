df818181c66637d2a6e6d5c428d3a027d50e8b7d
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 00:24:17 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,31 +1,26 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * _strstr - locates substring
- * @haystack: string to locte
- * @needle: substring to locate
- * Return: returns string at starting sub or null
- */
+*_strstr - first occurence
+*@haystack: main str
+*@needle: searched in haystack
+*Return: Always 0
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-	int j;
-
-	if (needle[0] == '\0')
-		return (haystack);
-
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		if (haystack[i] == needle[0])
-		{
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-					break;
-			}
-			if (needle[j] == '\0')
-				return (haystack + i);
-		}
-
-	}
-	return ('\0');
+char *str1, *str2;
+	
+while (*haystack != '\0')
+{
+str1 = haystack;
+str2 = needle;
+while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
+{
+haystack++;
+str2++;
+}
+if (*str2 == '\0')
+return (str1);
+haystack = str1 + 1;
+}
+return (0);
 }
