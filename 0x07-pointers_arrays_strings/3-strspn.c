9684a1094d53b03eea4a218a3b9670cefb01ba70
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 02:13:06 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* _strspn - prints the consecutive characters
-*@s: source string
-*@accept: searching string
-*Return: new string
-*/
+ * _strspn - length of prefix substring
+ * @s: string to go through
+ * @accept: accepted bytes
+ * Return: returns usigned values
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j;
-	
-for (j = 0; *(s + j); j++)
-{
-for (i = 0; *(accept + i); i++)
-{
-if(*(s + j) == *(accept + i))
-break;
-}
-if(*(accept + i) == '\0')
-break;
-}
-return (j);
+	int i;
+	int j;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				result++;
+				break;
+			}
+		}
+		if (s[j] == '\0')
+			return (result);
+	}
+	return (result);
 }
