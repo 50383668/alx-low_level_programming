2db96b4ba2fbed99efc69a1019fa9eee86303d37
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 02:36:18 +0300

    Create 0-main.c, main.h

---
+++ 0x03-debugging/0-main.c, main.h
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*main - test function
+*Return 0
+**/
+<<<<<<< HEAD
+
+int main(void)
+{
+	int i;
+	
+	i=0;
+	positive_or_negative(i);
+	
+	return (0);
+=======
+int main(void)
+{
+int i;
+i=0;
+positive_or_negative(i);
+	
+return (0);
+>>>>>>> b64440623d7d3319e8fae812135bc5f16e77d75a
+}
