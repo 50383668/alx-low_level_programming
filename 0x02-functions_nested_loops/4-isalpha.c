f578172e52994ad1073b5cd5e95d96c75cb847f4
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 01:45:07 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ *  * main ~ fucntion
+ *
+ *   * parameter
+ *
+ *    * c ~ isalpha variable
+ *
+ *     * Return (1) if alpha is true
+ *     */
+int _isalpha(int c)
+{
+	return ((c>='a' && c<='z')||(c>='A' && c<= 'Z'));
+}
