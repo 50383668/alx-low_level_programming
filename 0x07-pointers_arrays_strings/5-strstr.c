9143dd26630332d285d6705335f5411dc49e6a20
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 02:14:40 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,26 +1,31 @@
 #include "main.h"
 /**
-*_strstr - first occurence
-*@haystack: main str
-*@needle: searched in haystack
-*Return: Always 0
-*/
+ * _strstr - locates substring
+ * @haystack: string to locte
+ * @needle: substring to locate
+ * Return: returns string at starting sub or null
+ */
 char *_strstr(char *haystack, char *needle)
 {
-char *str1, *str2;
-	
-while (*haystack != '\0')
-{
-str1 = haystack;
-str2 = needle;
-while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
-{
-haystack++;
-str2++;
-}
-if (*str2 == '\0')
-return (str1);
-haystack = str1 + 1;
-}
-return (0);
+	int i;
+	int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+
+	}
+	return ('\0');
 }
