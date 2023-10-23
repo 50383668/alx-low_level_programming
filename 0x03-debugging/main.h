58477e50d55111313dcd3d0fb564a761597dd081
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 02:37:19 +0300

    Create main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+<<<<<<< HEAD
+int largest_number(int , int , int );
+void print_remaining_days(int month, int day , int year );
+int convert_day(int , int );
+=======
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+>>>>>>> b64440623d7d3319e8fae812135bc5f16e77d75a
+
+#endif /* MAIN_H */
