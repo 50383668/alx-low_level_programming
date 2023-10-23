a29dfe498d81d28a40c8ba0ce4a02ec91c1d8871
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 01:42:02 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *
+ * * main -Always execute
+ *
+ * *  print_alphabet-prints lowercase
+ *
+ *
+ *
+ * *Return: Always 0
+ *
+ * */
+
+void print_alphabet(void)
+
+{
+
+
+
+	int c;
+
+
+
+			for(c='a';c<='z';c++)
+
+					{
+
+								_putchar(c);
+
+									}
+			_putchar('\n');
+				
+
+}
