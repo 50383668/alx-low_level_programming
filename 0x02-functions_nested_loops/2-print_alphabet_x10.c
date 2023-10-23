8c8ec3275f67d16bea2cf207cf9de12e3a7d0732
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 01:43:05 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * main -execute
+ * Return :always 0
+ * print_alphabet_x10 -prints
+ * */
+void print_alphabet_x10(void)
+{
+	int j,x;
+	for(x=0;x<10;x++)
+	{
+		for(j='a';j<='z';j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
