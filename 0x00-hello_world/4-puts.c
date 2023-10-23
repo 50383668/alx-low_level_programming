583d6afa50647923cb9eabc6348fd850f0711f25
ann <113899485+50383668@users.noreply.github.com>
2023-10-24 00:49:22 +0300

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main function for string
+ */
+int main(void)
+{
+
+
+	char c[100]  =  "\"Programming is like building a multilingual puzzle";
+
+	puts(c);
+	return (0);
+}
